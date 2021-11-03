38672ca345fc5162ff11ad06ed6c7ae99c9093bb
Sevrinn <sevrinnsevrinn@gmail.com>
2021-11-03 06:29:52 -0700

    refactored  rendered links with a template string and styled

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let saveBtn = document.getElementById("save-btn")
 let inputEl = document.getElementById("input-el")
+let ulEl = document.getElementById("ul-el")
 let jobLeads = ["www.google.com", "www.sevwelker.com", "www.caskstore.com"]
 
 saveBtn.addEventListener("click", () => {
@@ -10,6 +11,7 @@ saveBtn.addEventListener("click", () => {
 
 const renderLeads = () => {
   for(let i = 0; i < jobLeads.length; i++) {
+    ulEl.innerHTML += `<li><a target="_blank" href='${jobLeads[i]}'>${jobLeads[i]}</a></li>`
     console.log(jobLeads[i])
   }
 }
