a8417120d9b4862a877040c6b623fd5cf3a52fb9
Sevrinn <sevrinnsevrinn@gmail.com>
2021-11-03 17:07:58 -0700

    leads save to localStorage

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+let jobLeads = []
 let saveBtn = document.getElementById("save-btn")
 let inputEl = document.getElementById("input-el")
 let ulEl = document.getElementById("ul-el")
-let jobLeads = ["www.google.com", "www.sevwelker.com", "www.caskstore.com"]
+let leadsInLocalStorage = JSON.parse(localStorage.getItem("jobLeads"))
+
+if(leadsInLocalStorage) {
+  jobLeads = leadsInLocalStorage
+  renderLeads()
+}
 
 saveBtn.addEventListener("click", () => {
   jobLeads.push(inputEl.value)
   inputEl.value = ""
+  localStorage.setItem("jobLeads", JSON.stringify(jobLeads))
   renderLeads()
 })
 
-const renderLeads = () => {
+function renderLeads() {
   let listItems = ""
   for(let i = 0; i < jobLeads.length; i++) {
     listItems += `
